e6e3609e8c4ec74941e526a2399da39ba11c020e
iosrule-hub <74600969+iosrule-hub@users.noreply.github.com>
2020-11-25 10:19:10 +0800

    Update ludingji.py

--- Ludingji/ludingji.py
+++ Ludingji/ludingji.py
@@ -1,1 +1,51 @@
-02490036
+import requests
+import os
+
+
+ludingji={"Accept": "*/*","Accept-Encoding": "br, gzip, deflate","Accept-Language": "zh-Hans-CN;q=1, en-US;q=0.9, zh-Hant-CN;q=0.8","As-Version": "v1","Content-Type": "application/json","User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 qapp","Version": "1211","Version-Name": ""}
+
+def ludingji(i,j,k):
+    print('=🔔='*k)
+    try:
+       response = requests.post(i,headers=ludingjivip,data=j)
+       print(response.text)
+    except Exception as e:
+       pass
+
+def watch(flag,list):
+   vip=''
+   if flag in os.environ:
+      vip = os.environ[flag]
+   if vip:
+       for line in vip.split('\n'):
+         if not line:
+            continue 
+         list.append(line.strip())
+       return list
+   else:
+       print(f'''【{flag}】 is empty,DTask is over.''')
+       exit()
+       
+
+
+
+
+def start():
+   funlist=[]
+   urllist=[]
+   tokenlist=[]
+   tklist= []
+   watch('ludingji_url',urllist)
+   watch('ludingji_tk',tklist)
+   watch('ludingji_token',tokenlist)
+   for i in range(8):
+      watch('ludingji_fun'+str(i),funlist)
+   for j in (len(tklist)):
+       ludingji['tk']=tklist[j]
+       ludingji['token']=tokenlist[j]
+       for k in range(8):
+           ludingji(urllist[k],funlist[k][j],(k+1))
+   print('🔔'*15)
+if __name__ == '__main__':
+       start()
+    
