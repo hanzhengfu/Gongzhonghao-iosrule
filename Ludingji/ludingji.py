dc97c60499e6bf4cff2ecb98845e08d668b61438
iosrule-hub <74600969+iosrule-hub@users.noreply.github.com>
2020-12-21 15:42:23 +0800

    Update ludingji.py

--- Ludingji/ludingji.py
+++ Ludingji/ludingji.py
@@ -3,9 +3,14 @@ import os
 import re
 import json
 import time
+import timeit
+import random
+import urllib
+from datetime import datetime
+
 osenviron={}
-
-
+djj_bark_cookie=''
+djj_sever_jiang=''
 
 
 
@@ -20,14 +25,20 @@ def ludingji(i,j,k):
              res=json.dumps(response.text)
              print(res)
              res=re.compile('(\d+.\d+)').findall(res)
-             res=res[0]+'-'+res[1]+'-'+res[2]
-             print('out',res)
+             res=res[0]+'|'+res[1]+'|'+res[2]
+             loger(res)
     except Exception as e:
       print(str(e))
       pass
 
 def watch(flag,list):
    vip=''
+   global djj_bark_cookie
+   global djj_sever_jiang
+   if "DJJ_BARK_COOKIE" in os.environ:
+     djj_bark_cookie = os.environ["DJJ_BARK_COOKIE"]
+   if "DJJ_SEVER_JIANG" in os.environ:
+      djj_sever_jiang = os.environ["DJJ_SEVER_JIANG"]
    if flag in osenviron:
       vip = osenviron[flag]
    if flag in os.environ:
@@ -40,12 +51,52 @@ def watch(flag,list):
        return list
    else:
        print(f'''【{flag}】 is empty,DTask is over.''')
-       exit()
-       
+       return
 
 
-
-
+def pushmsg(title,txt,bflag=1,wflag=1):
+   txt=urllib.parse.quote(txt)
+   title=urllib.parse.quote(title)
+   if bflag==1 and djj_bark_cookie.strip():
+      print("\n【通知汇总】")
+      purl = f'''https://api.day.app/{djj_bark_cookie}/{title}/{txt}'''
+      response = requests.post(purl)
+      #print(response.text)
+   if wflag==1 and djj_sever_jiang.strip():
+      print("\n【微信消息】")
+      purl = f'''http://sc.ftqq.com/{djj_sever_jiang}.send'''
+      headers={
+    'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+      body=f'''text={txt})&desp={title}'''
+      response = requests.post(purl,headers=headers,data=body)
+    #print(response.text)
+def loger(m):
+   print(m)
+   global result
+   result +=m+'\n'                
+def notice(b,e):
+    ll=False
+    start_time = datetime.strptime(str(datetime.now().date())+b, '%Y-%m-%d%H:%M')
+    end_time =  datetime.strptime(str(datetime.now().date())+e, '%Y-%m-%d%H:%M')
+    now_time = datetime.now()
+    if now_time > start_time and now_time<end_time:
+       ll=True
+    else:
+    	ll=False
+    return ll
+def clock(func):
+    def clocked(*args, **kwargs):
+        t0 = timeit.default_timer()
+        result = func(*args, **kwargs)
+        elapsed = timeit.default_timer() - t0
+        name = func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        print('[🔔运行完毕用时%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
+    
+@clock
 def start():
    urllist=[]
    tokenlist=[]
@@ -58,9 +109,18 @@ def start():
      funlist=[]
      watch('ludingji_fun'+str(i),funlist)
      alllist.append(funlist)
-   print(len(alllist))
+   #print(len(alllist))
    for max in range(25):
+     result=''
      jj=0
+     if(len(urllist)==0):
+        break
+     if(len(tklist)==0):
+        break
+     if(len(tokenlist)==0):
+        break
+     if(len(alllist)==0):
+        break
      for j in range(len(tklist)):
        jj+=1
        hd['tk']=tklist[j]
@@ -71,6 +131,7 @@ def start():
            time.sleep(2)
      time.sleep(10)
      print('💎'+str(max)+'=======')
+   pushmsg('ludingji',result)
 if __name__ == '__main__':
        start()
     
