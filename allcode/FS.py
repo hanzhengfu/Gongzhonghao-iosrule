a49b00bd279551251f4832027dc43371ea208ad2
iosrule-hub <74600969+iosrule-hub@users.noreply.github.com>
2021-02-03 19:19:29 +0800

    Delete FS.py

--- allcode/FS.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-new_dict={"code":200,"msg":"\u8bfb\u53d6\u52a9\u529b\u7801\u6210\u529f","data":["12333","2222","3333","ghhhjjj"],"powered by":"888888","sponsored by":"66666"}
-
-
-with open("./allcode/gc.json","w") as f:
-   json.dump(new_dict,f)
-   print("加载入文件完成...")
